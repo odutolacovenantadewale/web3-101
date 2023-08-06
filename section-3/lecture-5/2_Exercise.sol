14d74c2adb4e436beab6eb7133b89113470f0c17
odutolacovenantadewale <140220844+odutolacovenantadewale@users.noreply.github.com>
2023-08-07 00:21:49 +0100

    Update 2_Exercise.sol

--- section-3/lecture-5/2_Exercise.sol
+++ section-3/lecture-5/2_Exercise.sol
@@ -2,16 +2,12 @@
 
 pragma solidity ^0.8.16;
 
-/*
-Create a function that takes two arguments(x and y) of type uint and returns the value for x(x + y) and 
-store this number in a state variable. 
-*/
 contract Exercise {
     uint storedData; // State variable
 
-    // TODO: 
-    // Function calculation()
-    // Takes two arguments of type uint
-    // Returns the value for x(x+y)
-    // Also stores this value x(x+y) in the state variable storedData
+     function calculation(uint x, uint y) public returns (uint) {
+        uint result = x * (x + y);
+        storedData = result;
+        return result;
+     }
 }
