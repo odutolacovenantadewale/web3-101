01e19bc2408694cc36a78623e7f8c46cf9c90c78
odutolacovenantadewale <140220844+odutolacovenantadewale@users.noreply.github.com>
2023-08-07 00:19:13 +0100

    Update 2_Exercise.sol

--- section-3/lecture-5/2_Exercise.sol
+++ section-3/lecture-5/2_Exercise.sol
@@ -10,8 +10,12 @@ contract Exercise {
     uint storedData; // State variable
 
     // TODO: 
-    // Function calculation()
-    // Takes two arguments of type uint
-    // Returns the value for x(x+y)
-    // Also stores this value x(x+y) in the state variable storedData
+    function calculation(unit x, unit y) public returns (uint) {
+          uint result = x * (x + y);
+        storedData = result;
+        return result;
+    }
 }
+    
+
+
